aef34865024976943e3e94e0e0ce09a3b462acbf
Luara Chambers <89796476+Luara-Chambers@users.noreply.github.com>
2021-10-28 14:47:15 -0600

    added homework 9

---
+++ Homework9/js/sketch.js
@@ -0,0 +1,32 @@
+function setup()
+{
+  createCanvas(500,600);
+}
+function draw()
+{
+  background(255,105,0);
+  textSize(22)
+  text("Happy Halloween!", 150,100);
+
+  fill(265,160,250);
+  circle(240,240,100);
+  line(201,204,191,196);
+  line(221,194,211,186);
+  line(241,187,241,176);
+  line(261,194,271,186);
+  line(281,204,291,196);
+  strokeWeight(5);
+  point(230,230);
+  point(250,230);
+  triangle(240,250,241,245,242,250);
+  ellipse(240,265,20,10);
+  line(241,290,241,450);
+  line(191,350,291,350);
+  circle(191,350,30);
+  circle(291,350,30);
+  line(241,450,191,550);
+  line(241,450,291,550);
+  rect(171,540,30,20);
+  rect(281,540,30,20);
+  text("Luara Chambers",150,50);
+}
